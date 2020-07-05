aa43f20a8152a9d1dc228d18eb8a50b7695e0c1a
Pavan Baddi <pavanbaddi911@gmail.com>
2020-07-05 09:59:25 +0530

    minor

---
+++ one/my_script.py
@@ -0,0 +1,24 @@
+from my_constants import (PI, MAX_YEAR, MIN_YEAR, )
+
+# print(PI)
+# print(MAX_YEAR)
+# print(MIN_YEAR)
+
+# foo = bar = 25
+
+# print("foo", foo)
+# print("bar", bar)
+
+# # PYTHON VARIABLES
+# foo 25
+# bar 25
+
+# jack, john, jim = "Hi I'm Jack",  "Hi I'm John", "Hi I'm Jim"
+# print(jack)
+# print(john)
+# print(jim)
+
+# PYTHON VARIABLES
+# Hi I'm Jack
+# Hi I'm John
+# Hi I'm Jim
